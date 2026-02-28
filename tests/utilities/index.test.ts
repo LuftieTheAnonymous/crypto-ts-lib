070b4e950374c9aaa037833421d9fd322bee7ed8
Luftjunkie19 <luftjunkie@protonmail.com>
2026-02-28 12:03:33 +0100

    remove unused tests

--- tests/utilities/index.test.ts
+++
@@ -1,6 +0,0 @@
-import {expect} from 'chai';
-import { modularMath } from '../../index';
-
-    expect(modularMath.XOR([34])).is("0").ok('success', 'the expected output is correct');
-
-    expect(modularMath.XOR([13])).is("1").ok('success', 'the expected output is correct');
